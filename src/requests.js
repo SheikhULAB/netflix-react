9f03ab26c9db12f5ebb17a1bae76a42ced1f1d0b
SheikhULAB <tosibalaptop86@gmail.com>
2022-04-25 22:28:57 +0600

    first commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "4e43e95c3232ef735bcc60c94da52760";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  };
+  
+  export default requests;
